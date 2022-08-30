44aab3408f0024e181d0490fb02df6f7a32ff3ed
Vaibhav <vaibhavshukla2704@gmail.com>
2022-08-30 17:09:58 +0530

    successfully connect mongodb

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const MONGODB_URL =
+  "mongodb+srv://Vaibhav:admin@cluster0.9v5tjpp.mongodb.net/tour_db?retryWrites=true&w=majority";
+
+const port = 5000;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not Connect`));
