8b84458f1ffb1795db2c3ac3f2e434a64b26991c
Vaibhav <vaibhavshukla2704@gmail.com>
2022-08-31 18:41:57 +0530

    work on sign up

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/users", userRouter);
 
 const MONGODB_URL =
   "mongodb+srv://Vaibhav:admin@cluster0.9v5tjpp.mongodb.net/tour_db?retryWrites=true&w=majority";
---
+++ server/routes/userRoute.js
@@ -0,0 +1,7 @@
+import express from "express";
+const router = express.Router();
+
+import { signup } from "../controllers/userControllers";
+// router.post("/signup", signup);
+
+export default router;
