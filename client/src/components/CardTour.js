c99a7cd8e2f64ca6020450e4d41ebd1b7641c82b
Vaibhav <vaibhavshukla2704@gmail.com>
2022-09-03 14:05:19 +0530

    working on cardtour module :

---
+++ client/src/components/CardTour.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {} from "mdb-react-ui-kit";
+import {} from "react-router-dom";
+
+const CardTour = ({ imageFile, description, title, tags, _id, name }) => {
+  return <div>CardTour</div>;
+};
+
+export default CardTour;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBTypography } from "mdb-react-ui-kit";
+import { useSelector, useDispatch } from "react-redux";
+import { getTours } from "../redux/features/tourSlice";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { tours, loading } = useSelector((state) => ({ ...state.tour }));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTours());
+  }, []);
+
+  if (loading) {
+    return <h2>Loading.......</h2>;
+  }
+
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "1000px",
+        alignContent: "center",
+      }}
+    >
+      <MDBRow className="mt-5">
+        {tours.length === 0 && (
+          <MDBTypography className="text-center mb-0" tag="h2">
+            No Tours Found
+          </MDBTypography>
+        )}
+        <MDBCol>
+          <MDBContainer>
+            <MDBRow className="row-cols-1 row-cols-md-3 g-2 mt-4">
+              {tours && tours.map((item, index) => <h2>Tour Card</h2>)}
+            </MDBRow>
+          </MDBContainer>
+        </MDBCol>
+      </MDBRow>
+    </div>
+  );
 };
 
 export default Home;
